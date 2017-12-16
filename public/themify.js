74a73a53d3ee0b75be757a0f5be3ffa17a45f510
cliveb <clive.boulton@gmail.com>
2017-12-16 12:48:15 -0800

    revert to bricks (Caja boxes)

--- public/themify.js
+++ public/themify.js
@@ -1,4 +1,4 @@
-(function () { function a(a, b, c) { return "#" + ((256 + a << 8 | b) << 8 | c).toString(16).slice(1); } function b(a, b, c) { return [a[0] * (1 - c) + b[0] * c, a[1] * (1 - c) + b[1] * c, a[2] * (1 - c) + b[2] * c]; } !~navigator.platform.indexOf("Linux") || $("body").addClass("etc"); var c = 200, d = 120, e, f, g = [[228, 114, 42], [178, 47, 18], [237, 146, 40], [26, 155, 126]]; for (var h = 0, i = 0; i < screen.height; h++, i += e + 2) {
+(function () { function a(a, b, c) { return "#" + ((256 + a << 8 | b) << 8 | c).toString(16).slice(1); } function b(a, b, c) { return [a[0] * (1 - c) + b[0] * c, a[1] * (1 - c) + b[1] * c, a[2] * (1 - c) + b[2] * c]; } !~navigator.platform.indexOf("Linux") || $("body").addClass("etc"); var c = 120, d = 200, e, f, g = [[228, 114, 42], [178, 47, 18], [237, 146, 40], [26, 155, 126]]; for (var h = 0, i = 0; i < screen.height; h++, i += e + 2) {
     e = c, h == 1 ? e = 230 : h > 1 && (e = Math.floor(e * .7 + Math.random() * e * .4));
     for (var j = 0, k = 0; k < screen.width; j++, k += f + 2) {
         f = d + Math.random() * 20 - Math.random() * 20, f = Math.floor(f * .8 + Math.random() * f * .5);
